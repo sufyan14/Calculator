3fcf208af68d0f218396efff1f84d51b1e02b5e6
Muhammad Sufyan <muhammadsufyan00ms@gmail.com>
2022-12-04 18:22:25 +0500

    updated ui/ux

--- Calculator.js
+++ Calculator.js
@@ -1,14 +1,34 @@
-function clr()
-{
-    document.getElementById("result").value = " ";
-}
-function display(val)
-{
-    document.getElementById("result").value+=val;
-}
-function equate()
-{
-    let x = document.getElementById("result").value;
-    let y = eval(x);
-    document.getElementById("result").value = y;
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'clear'){
+            display.innerText = '';
+        }
+        else if(item.id == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        }
+        else if(display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        }
+        else if(display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!!';
+            setTimeout(() =>(display.innerText = '')
+            ,2000);
+        }
+        else{
+            display.innerText += item.id;
+        }
+    }
+})
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calc = document.querySelector('.dark');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+themeToggleBtn.onclick = () => {
+    calc.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+    isDark = !isDark;
 }
